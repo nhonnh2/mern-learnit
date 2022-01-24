cd915cd0639a536f83d8dc0e1fdb0f9f56538a36
Nguyen Huu Nhon <huunhon099@gmail.com>
2022-01-24 12:37:58 +0700

    about

--- client/src/views/About.js
+++ client/src/views/About.js
@@ -1,9 +1,16 @@
+import { GithubOutlined } from '@ant-design/icons';
 import React from 'react';
 
 function About() {
   return (
     <div>
-      <h1>About</h1>
+      <a
+        href="https://github.com/nhonnh2/mern-learnit"
+        target="blank"
+        className="flex justify-center items-center text-white bg-indigo-500 py-2 mt-8 text-base rounded-md w-[80%] md:w-[60%] lg:w-[45%] m-auto"
+      >
+        Visit my github source code <GithubOutlined className="ml-2" />
+      </a>
     </div>
   );
 }
